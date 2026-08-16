9c0302fbcd2b0522cfb7f720e5b6b53277739cce
aleck2209 <aleckyann09@gmail.com>
2026-08-16 09:10:18 +0100

    feat: mise en place du chargement et du message d'erreur. réalisation de la structure html pour le filtre et la recherche d'article par titre.

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 // Variables
-const baseUrl = 'https://fakestoreapi.com'
-const sectionProducts = document.getElementById('products')
+const baseUrl = "https://fakestoreapi.com";
+const sectionProducts = document.getElementById("products");
 
 // Recupérer les produits
 const getProducts = async () => {
-    try {
-        const response = await fetch(`${baseUrl}/products`);
-        const data = response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
-        return 'Chargement des données'
-    }   
-}
+    const response = await fetch(`${baseUrl}/products`);
+    if (!response.ok) {
+        throw new Error(`Erreur HTTP : ${response.status}`)
+    }
+
+    const data = response.json();
+    return data;
+};
 
 const displayProducts = async () => {
+  try {
+    // Etat de chargement
+    sectionProducts.innerHTML = `<p class="loader">Chargement en cours...</p>`;
     const data = await getProducts();
-    console.log(data);
-    sectionProducts.innerHTML = data.map(item => 
-    `
-        <article class="card-product">
-            <figure class="img-container">
-                <img class="img-article" src="${item.image}" alt="${item.title}"/>
-            </figure>
-            <div class="text-container-product">
-                <div class="price-category-container">
-                    <p class="price-card">$${item.price}</p>
-                    <span class="category-card">${item.category}</span>
-                </div>
-                <p class="title-card">${item.title}</p>
-                <p class="description-card">${item.description.substring(0, 100)}...</p>
-            <div/>
-        </article>
-    `).join('')
-}
+
+    // Chargement reussi
+    if (data) {
+      sectionProducts.classList.replace('block', 'products');
+      sectionProducts.innerHTML = data
+        .map(
+          (item) =>
+            `
+            <article class="card-product">
+                <figure class="img-container">
+                    <img class="img-article" src="${item.image}" alt="${item.title}"/>
+                </figure>
+                <div class="text-container-product">
+                    <div class="price-category-container">
+                        <p class="price-card">$${item.price}</p>
+                        <span class="category-card">${item.category}</span>
+                    </div>
+                    <p class="title-card">${item.title}</p>
+                    <p class="description-card">${item.description.substring(0, 100)}...</p>
+                <div/>
+            </article>
+        `,
+        )
+        .join("");
+    }
+  } catch (error) {
+    sectionProducts.innerHTML = `<p class="error">Impossible de charger les produits.<br>Veuillez vérifier votre connexion et réessayer.</p>`;
+  }
+};
 
 displayProducts();
